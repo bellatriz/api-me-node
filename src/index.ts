e04d5452576cab4a2952e69bc7a3e035f8f12e2d
bellatriz <betiz.menezes@gmail.com>
2022-05-25 20:09:37 -0400

    Create articles router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import articlesRouter from './routers/articles-router'
 
+// Porta do servidor
 const PORT = process.env.PORT || 4000
 
+// Host do servidor
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 
 const app = express()
 
+// Endpoint raiz
 app.get('/', (req, res) => {
   res.send('Back-end Challenge 2021 🏅 - Space Flight News')
 })
@@ -15,10 +19,15 @@ app.use(cors({
   origin: ['http://localhost:3000']
 }))
 
+// Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {
   res.status(404)
 })
 
+// Rotas
+app.use('/api', articlesRouter)
+
+// Inicia o sevidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
 })
